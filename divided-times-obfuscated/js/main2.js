1313cbd753fce1d0a881f99533f26002979aea0f
Brian Ries <bkries@gmail.com>
2015-07-23 18:51:02 -0400

    Added 3 conditions
    Guidance from Seb for GA

--- divided-times-obfuscated/js/main2.js
+++ divided-times-obfuscated/js/main2.js
@@ -5,14 +5,33 @@ $(document).ready(function () {
 
 		//this handles slidedown
 
-		$(this).addClass('active');
-		$(this).siblings().removeClass('active')
+			//if (slide-down is not open -- use '!' for opposite)
+			
+			if(!$('#slide-down').hasClass('open')) {
 
-			//if (slide-down is not open)
+				//show slide-down
 
-			//else if(slide-down is open and link has active class)
+				//slide-down.addClass('open')
+
+				//active class for li
+			}
+
+			//else if(slide-down is open and link has active class) -- use && for 'and'
+
+			else if($('#slide-down').hasClass('open' && $(this).hasClass('active')) {
+
+				//hide slide-down
+
+				//slide-down.removeClass('open')
+
+				//active class for li
+			}
 
 			//else (clicked on a non-active link while slide-down is open)
+
+			else {
+
+			}
 
 		//this handles primary nav active
 
